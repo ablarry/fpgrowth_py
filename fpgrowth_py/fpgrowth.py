edb3b1fbfda564f94f2c47229a318da56ce1b59e
Abraham Larrazolo <larrazolo.abraham@gmail.com>
2021-10-18 18:47:08 -0500

    Add graphviz

--- fpgrowth_py/fpgrowth.py
+++ fpgrowth_py/fpgrowth.py
@@ -3,6 +3,7 @@ from csv import reader
 from itertools import chain, combinations
 from optparse import OptionParser
 from fpgrowth_py.utils import *
+from graphviz import Graph as Graphviz
 
 def fpgrowth(itemSetList, minSupRatio, minConf):
     frequency = getFrequencyFromList(itemSetList)
@@ -21,6 +22,7 @@ def fpgrowthFromFile(fname, minSupRatio, minConf):
     #minSup = len(itemSetList) * minSupRatio
     minSup = minSupRatio
     fpTree, headerTable = constructTree(itemSetList, frequency, minSup)
+    #createGraphviz(fpTree)
     if(fpTree == None):
         print('No frequent item set')
     else:
@@ -30,6 +32,22 @@ def fpgrowthFromFile(fname, minSupRatio, minConf):
         #rules = associationRule(freqItems, itemSetList, minConf)
         return freqItems, rules
 
+def createGraphviz(tree):
+    dot = Graphviz()
+    for k in tree.children:
+        dot.edge('NULL', k)
+        createGraphvizChildren(k,tree.children[k],dot)
+    print(dot.source)
+    dot.view()
+
+def createGraphvizChildren(parent, parent_node, dot):
+    for count,i in enumerate(parent_node.children):
+        if count > 3:
+            break
+        dot.edge(parent, i)
+        if len(parent_node.children[i].children) != 0:
+            createGraphvizChildren(i, parent_node.children[i], dot)
+    return
 if __name__ == "__main__":
     optparser = OptionParser()
     optparser.add_option('-f', '--inputFile',
